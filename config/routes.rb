fbdcc2eab435c55b7a474874177324527689df5c
tolaseadegbite <tolasekelvin@gmail.com>
2023-10-16 10:24:03 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'quiz_submissions/create'
+  get 'quiz_submissions/show'
+
+  resources :quizzes do
+    resources :questions
+    # post 'submit', to: 'quiz_submissions#create'
+    resources :quiz_submissions, only: [:show, :create], as: 'submissions'
+  end
+  
+  root 'quizzes#index'
+end
